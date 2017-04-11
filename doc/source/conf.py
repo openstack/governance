ba71647bf3edc3cd42c241521ce65307333b269c
Monty Taylor <mordred@inaugust.com>
2017-04-11 05:57:23 -0500

    Remove empty man_pages setting
    This breaks with sphinx warnings as errors.
    
    Change-Id: Ib032885003b17e70bf3fc9768ffc4383b834d2ff

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -222,8 +222,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-]
+#man_pages = []
 
 # If true, show URL addresses after external links.
 #man_show_urls = False
